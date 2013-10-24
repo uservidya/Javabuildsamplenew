da4e21f57460d1333b99e5b9282fad26cfb2c2af
vidyaraghav <vidyac.raghav@gmail.com>
2013-10-24 15:16:47 +0530

    Update TestMath.java

--- src/TestMath.java
+++ src/TestMath.java
@@ -14,7 +14,7 @@ public class TestMath extends TestCase {
     
   public void testAdd() {
   	int num1 = 3;
-  	int num2 = 2;
+  	int num2 = 3;
   	int total = 5;
   	int sum = 0;
   	sum = Math.add(num1, num2);
